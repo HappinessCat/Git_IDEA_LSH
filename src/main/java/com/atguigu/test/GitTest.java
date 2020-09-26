7b87cf2ecbfcbce769bbaeb03242e3ac4fd3c571
lishihao <1424195085@qq.com>
2020-09-27 00:07:27 +0800

    do git commit

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -5,7 +5,7 @@ package com.atguigu.test;
  */
 public class GitTest {
     public static void main(String[] args) {
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("I'm Iron man!");
         }
     }
