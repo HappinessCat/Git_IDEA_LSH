6d7fc93394f63f7f80cbb8e33d38e587c8e3d5ba
lishihao <1424195085@qq.com>
2020-09-27 00:12:48 +0800

    第二次测试:从master修改commit提交

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -1,12 +1,11 @@
 package com.atguigu.test;
 
-/**
- * Created by Jason on 2020/9/26 0026.
- */
+
 public class GitTest {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("I'm Iron man!");
+            System.out.println("Love Three Thousands !");
         }
     }
 }
