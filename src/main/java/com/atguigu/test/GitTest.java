946ef9dffeb9a7c58a23f479e93086d3b8673b10
lishihao <1424195085@qq.com>
2020-09-27 00:01:03 +0800

    do git commit

---
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.test;
+
+/**
+ * Created by Jason on 2020/9/26 0026.
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            System.out.println("I'm Iron man!");
+        }
+    }
+}
